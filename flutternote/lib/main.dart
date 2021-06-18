17e80f522a32900aa3539ad0a5a3135a0051fd29
Ankan Sharma <ankanroni3@gmail.com>
2021-06-18 23:05:57 +0530

    Added Note_detail in the App

--- flutternote/lib/main.dart
+++ flutternote/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutternote/screens/note_detail.dart';
 import 'package:flutternote/screens/note_list.dart';
 
 void main() {
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.amber,
       ),
-      home: NoteList(),
+      home: NoteDetail(),
     );
   }
 }
---
+++ flutternote/lib/screens/note_detail.dart
@@ -0,0 +1,134 @@
+import 'package:flutter/material.dart';
+
+class NoteDetail extends StatefulWidget {
+  const NoteDetail({Key? key}) : super(key: key);
+
+  @override
+  _NoteDetailState createState() => _NoteDetailState();
+}
+
+class _NoteDetailState extends State<NoteDetail> {
+  static var _priorities = ['Urgent', 'Medium', 'Casual'];
+  TextEditingController titleController = TextEditingController();
+  TextEditingController descriptionController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    TextStyle? textStyle = Theme.of(context).textTheme.headline5;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Edit Notes',
+        ),
+      ),
+      body: Padding(
+        padding: EdgeInsets.only(top: 15.0, left: 10.0, right: 10.0),
+        child: ListView(
+          children: <Widget>[
+            ListTile(
+              title: DropdownButton(
+                items: _priorities.map((String dropDownStringItem) {
+                  return DropdownMenuItem<String>(
+                    child: Text(dropDownStringItem),
+                    value: dropDownStringItem,
+                  );
+                }).toList(),
+                style: textStyle,
+                value: 'Casual',
+                onChanged: (value) {
+                  setState(() {
+                    debugPrint('user ${value}');
+                  });
+                },
+              ),
+            ),
+            // Second Element
+            Padding(
+              padding: EdgeInsets.only(
+                top: 15.0,
+                bottom: 15.0,
+              ),
+              child: TextField(
+                controller: titleController,
+                style: textStyle,
+                onChanged: (value) {
+                  debugPrint("Something title text field");
+                },
+                decoration: InputDecoration(
+                    labelText: 'Title',
+                    labelStyle: textStyle,
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(5.0))),
+              ),
+            ),
+            // Third element
+            Padding(
+              padding: EdgeInsets.only(
+                top: 15.0,
+                bottom: 15.0,
+              ),
+              child: TextField(
+                controller: titleController,
+                style: textStyle,
+                onChanged: (value) {
+                  debugPrint("Something title text field");
+                },
+                decoration: InputDecoration(
+                    labelText: 'Title',
+                    labelStyle: textStyle,
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(5.0))),
+              ),
+            ),
+            // Fourth element
+            Padding(
+                padding: EdgeInsets.only(
+                  top: 15.0,
+                  bottom: 15.0,
+                ),
+                child: Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: FloatingActionButton.extended(
+                        onPressed: () {
+                          setState(() {
+                            debugPrint('Save clicked');
+                          });
+                        },
+                        tooltip: 'Save Note',
+                        icon: Icon(
+                          Icons.save,
+                        ),
+                        backgroundColor: Colors.amber,
+                        foregroundColor: Colors.black87,
+                        label: Text('Save'),
+                      ),
+                    ),
+
+                    Container(width: 5.0,),
+
+                    Expanded(
+                      child: FloatingActionButton.extended(
+                        onPressed: () {
+                          setState(() {
+                            debugPrint('delete clicked');
+                          });
+                        },
+                        tooltip: 'Delete',
+                        icon: Icon(
+                          Icons.delete_forever,
+                        ),
+                        backgroundColor: Colors.amber,
+                        foregroundColor: Colors.black87,
+                        label: Text('Delete'),
+                      ),
+                    )
+                  ],
+                ))
+          ],
+        ),
+      ),
+    );
+  }
+}
