fbe549e02b86260e2b01c0150f6d5aade6230399
Ankan Sharma <ankanroni3@gmail.com>
2021-06-19 20:56:00 +0530

    Added model.dart added defined values for DB, added getter, setter and added toMap to convert to map object as required by sqlite DB for transfer of data

---
+++ flutternote/lib/models/note.dart
@@ -0,0 +1,52 @@
+class Note {
+  late int _id;
+  late String _title;
+  late String _description;
+  late String _date;
+  late int _priority;
+
+  Note(this._date, this._id, this._priority, this._title, this._description);
+
+  Note.withId(
+      this._id, this._date, this._priority, this._title, this._description);
+
+  int get id => _id;
+  String get title => _title;
+  String get description => _description;
+  String get date => _date;
+  int get priority => _priority;
+
+  set title(String newTitle) {
+    if (newTitle.length <= 255) {
+      this._title = newTitle;
+    }
+  }
+
+  set description(String newdescription) {
+    this._description = newdescription;
+  }
+
+  set priority(int newPriority) {
+    this._priority = newPriority;
+  }
+
+  set date(String newDate) {
+    this._date = newDate;
+  }
+
+  // Convert note object to map object
+
+  Map<String, dynamic> toMap() {
+    var map = Map<String, dynamic>();
+
+    if (id != null) {
+      map["id"] = _id;
+    }
+    map["title"] = _title;
+    map["description"] = _description;
+    map["priority"] = _priority;
+    map["date"] = _date;
+
+    return map;
+  }
+}
