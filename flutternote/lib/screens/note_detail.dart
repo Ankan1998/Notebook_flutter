15eb2964840c9d500bd82f0dcd35b6b5d4b7f616
Ankan Sharma <ankanroni3@gmail.com>
2021-06-27 21:43:46 +0530

    Completed priority updation in note_details.dart

--- flutternote/lib/screens/note_detail.dart
+++ flutternote/lib/screens/note_detail.dart
@@ -1,29 +1,67 @@
 import 'package:flutter/material.dart';
+import 'package:flutternote/models/note.dart';
 import 'package:flutternote/screens/note_detail.dart';
+import 'package:flutternote/utils/database_helper.dart';
 
 // ignore: must_be_immutable
 class NoteDetail extends StatefulWidget {
   //const NoteDetail(String title, {Key? key}) : super(key: key);
-  String barTitle;
-  NoteDetail(this.barTitle);
+  final String barTitle;
+  final Note note;
+  NoteDetail(this.note, this.barTitle);
 
   @override
-  _NoteDetailState createState() => _NoteDetailState();
+  _NoteDetailState createState() => _NoteDetailState(this.note, this.barTitle);
 }
 
 class _NoteDetailState extends State<NoteDetail> {
   static var _priorities = ['Urgent', 'Medium', 'Casual'];
   TextEditingController titleController = TextEditingController();
   TextEditingController descriptionController = TextEditingController();
+  String barTitle;
+  Note note;
+  _NoteDetailState(this.note, this.barTitle);
+  DatabaseHelper helper = DatabaseHelper();
 
   void moveToMainScreen() {
     Navigator.pop(context);
   }
 
+  void updateIntPriority(String value) {
+    switch (value) {
+      case 'Urgent':
+        note.priority = 1;
+        break;
+      case 'Medium':
+        note.priority = 2;
+        break;
+      case 'Casual':
+        note.priority = 3;
+        break;
+    }
+  }
+
+  String updateStringPriority(int value) {
+    String priority;
+    switch (value) {
+      case 1:
+        priority = _priorities[0];
+        break;
+      case 2:
+        priority = _priorities[1];
+        break;
+      case 3:
+        priority = _priorities[2];
+        break;
+    }
+    return priority;
+  }
+
   @override
   Widget build(BuildContext context) {
     TextStyle textStyle = Theme.of(context).textTheme.headline5;
-
+    titleController.text = note.title;
+    descriptionController.text = note.description;
     return Scaffold(
       resizeToAvoidBottomInset: true,
       appBar: AppBar(
@@ -40,7 +78,7 @@ class _NoteDetailState extends State<NoteDetail> {
                 items: _priorities.map((String dropDownStringItem) {
                   return DropdownMenuItem<String>(
                     child: Text(dropDownStringItem),
-                    value: dropDownStringItem,
+                    value: updateStringPriority(note.priority),
                   );
                 }).toList(),
                 style: textStyle,
@@ -48,6 +86,7 @@ class _NoteDetailState extends State<NoteDetail> {
                 onChanged: (value) {
                   setState(() {
                     debugPrint('user ${value}');
+                    updateIntPriority(value);
                   });
                 },
               ),
