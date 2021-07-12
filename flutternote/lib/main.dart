650c1d185af9903fc232cc3a5afe0f02842b9029
Ankan Sharma <ankanroni3@gmail.com>
2021-07-13 02:27:21 +0530

    Opening with Login page added

--- flutternote/lib/main.dart
+++ flutternote/lib/main.dart
@@ -1,23 +1,48 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutternote/screens/login.dart';
 //import 'package:flutternote/screens/note_detail.dart';
 import 'package:flutternote/screens/note_list.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'NoteSet',
+  runApp(
+    MaterialApp(
+      //   localizationsDelegates: <LocalizationsDelegate<dynamic>>[
+      //   DefaultMaterialLocalizations.delegate,
+      //   DefaultWidgetsLocalizations.delegate,
+      //   DefaultCupertinoLocalizations.delegate,
+      // ],
       theme: ThemeData(
         primarySwatch: Colors.amber,
       ),
-      home: NoteList(),
-    );
-  }
+      home: LoginPage(),
+      routes: {
+        NoteList.routeName: (ctx) => NoteList(),
+        // AddReminder.routeName: (ctx) => AddReminder(),
+        // LoginPage.routeName: (ctx) => LoginPage(),
+        
+        //SettingPage.routeName: (ctx) => SettingPage(),
+        //LocalizedBird.routeName: (ctx) => LocalizedBird()
+      },
+    ),
+  );
 }
+
+// class MyApp extends StatelessWidget {
+//   // This widget is the root of your application.
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'NoteSet',
+//       theme: ThemeData(
+//         primarySwatch: Colors.amber,
+//       ),
+//       home: NoteList(),
+//     );
+//   }
+// }
 
 
