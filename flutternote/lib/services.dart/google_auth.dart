a51694effa959c5b35016e2d9230f173c113e920
Ankan Sharma <ankanroni3@gmail.com>
2021-07-13 13:51:02 +0530

    Change in home screen after logging in

--- flutternote/lib/services.dart/google_auth.dart
+++ flutternote/lib/services.dart/google_auth.dart
@@ -4,6 +4,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutternote/screens/note_list.dart';
+import 'package:flutternote/widgets/home.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 //import 'package:mynotes/pages/homepage.dart';
 class AuthService {
@@ -45,13 +46,13 @@ class AuthService {
             // old user
             doc.reference.update(userData);
 
-            Navigator.pushReplacementNamed(context, NoteList.routeName);
+            Navigator.pushReplacementNamed(context, NoteApp.routeName);
           } else {
             // new user
 
             users.doc(user.uid).set(userData);
 
-            Navigator.pushReplacementNamed(context, NoteList.routeName);
+            Navigator.pushReplacementNamed(context, NoteApp.routeName);
           }
         });
       }
