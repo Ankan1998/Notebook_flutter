ae07718d72b4f4f54d3eec44bb152ff7501931e6
Ankan Sharma <ankanroni3@gmail.com>
2021-07-13 16:03:18 +0530

    update added in firestore service

--- flutternote/lib/services.dart/firestore_service.dart
+++ flutternote/lib/services.dart/firestore_service.dart
@@ -2,7 +2,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutternote/models/note.dart';
 
-
 class FireStore_srv {
   void add_fs(Note newnote) async {
     // save to db
@@ -15,7 +14,7 @@ class FireStore_srv {
       'title': newnote.title,
       'des': newnote.description,
       'datetime': newnote.date,
-      'priority' : newnote.priority
+      'priority': newnote.priority
     };
 
     ref.add(data);
@@ -23,13 +22,12 @@ class FireStore_srv {
     //
   }
 
-    void add_log(Map<dynamic, dynamic> data) async {
+  void add_log(Map<dynamic, dynamic> data) async {
     // save to db
     CollectionReference ref_log = FirebaseFirestore.instance
         .collection('users')
         .doc(FirebaseAuth.instance.currentUser.uid)
         .collection('logged');
-
 
     ref_log.add(data);
 
@@ -39,5 +37,14 @@ class FireStore_srv {
   void deleteReminder(DocumentReference ref) async {
     // delete from db
     await ref.delete();
+  }
+
+  void update(DocumentReference ref, Note note) async {
+    await ref.update({
+      'title': note.title,
+      'des': note.description,
+      'datetime': note.date,
+      'priority': note.priority
+    });
   }
 }
