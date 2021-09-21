49552e0151b0b62f5d3f1e0a8422616ca6a53c66
Ankan Sharma <36896102+Ankan1998@users.noreply.github.com>
2021-09-21 20:21:35 +0530

    hive initialised in main

--- flutternote/lib/main.dart
+++ flutternote/lib/main.dart
@@ -1,31 +1,45 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 //import 'package:flutternote/screens/note_detail.dart';
-import 'package:flutternote/screens/note_list.dart';
+import 'package:flutternote/screens/splash_screen.dart';
+import 'package:flutternote/themes.dart';
 import 'package:hive/hive.dart';
 import 'package:path_provider/path_provider.dart' as path_provider;
 
-void main() async {
-
-  // get the path for hive storage
-  final appDocumentDirectory =
-      await path_provider.getApplicationDocumentsDirectory();
-  // Init the hive
-  Hive.init(appDocumentDirectory.path);
+void main() {
   runApp(MyApp());
-  // open the hive box
-  final notebox = await Hive.openBox('notes');
+  // // open the hive box
+  // final notebox = await Hive.openBox('notes');
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+    hiveinit();
+  }
+
+  Future<void> hiveinit() async {
+    final appDocumentDirectory =
+        await path_provider.getApplicationDocumentsDirectory();
+    Hive.init(appDocumentDirectory.path);
+    final notebox = await Hive.openBox('notes');
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Notie',
       theme: ThemeData(
-        primarySwatch: Colors.amber,
+        primaryColor: MyTheme.kPrimaryColor,
       ),
-      home: NoteList(),
+      home: SplashScreenx(),
     );
   }
 }
