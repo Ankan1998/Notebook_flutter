eac83a799bba67b8088ecde2f1d3998dc7504ab0
Ankan Sharma <36896102+Ankan1998@users.noreply.github.com>
2021-09-24 21:30:42 +0530

    Note detail page added to see the note

---
+++ flutternote/lib/screens/note_detail.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:flutternote/models/note.dart';
+import 'package:flutternote/themes.dart';
+
+class NoteDetail extends StatefulWidget {
+  // NoteDetail({Key? key}) : super(key: key);
+  final notedetail;
+
+  const NoteDetail({Key key, this.notedetail}) : super(key: key);
+
+  @override
+  _NoteDetailState createState() => _NoteDetailState();
+}
+
+class _NoteDetailState extends State<NoteDetail> {
+  @override
+  Widget build(BuildContext context) {
+    
+    return Scaffold(
+        backgroundColor: MyTheme.backgroundColor,
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).primaryColor,
+          title: Text(widget.notedetail.title),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(25.0),
+          child: Container(
+            
+            width: MediaQuery.of(context).size.width * 0.95,
+            height: MediaQuery.of(context).size.height * 0.81,
+            child: Padding(
+              padding: const EdgeInsets.all(25.0),
+              child: SingleChildScrollView(child: Text(widget.notedetail.description)),
+            ),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              
+              borderRadius: BorderRadius.all(Radius.circular(30))
+            ),
+          )));
+  }
+}
