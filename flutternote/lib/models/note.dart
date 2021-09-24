73f6bcc613f87eec3bf469043e80646265e2c31b
Ankan Sharma <36896102+Ankan1998@users.noreply.github.com>
2021-09-24 17:02:34 +0530

    note.dart updated with hive type adapter

--- flutternote/lib/models/note.dart
+++ flutternote/lib/models/note.dart
@@ -1,64 +1,18 @@
+import 'package:hive/hive.dart';
+part 'note.g.dart';
 
+@HiveType(typeId: 0)
 class Note {
-  int _id;
-  String _title;
-  String _description;
-  String _date;
-  int _priority;
+  @HiveField(0)
+  int id;
+  @HiveField(1)
+  String title;
+  @HiveField(2)
+  String description;
+  @HiveField(3)
+  String date;
+  @HiveField(4)
+  int priority;
 
-  Note(this._title, this._date, this._priority, [this._description]);
-
-  Note.withId(
-      this._id, this._title, this._date, this._priority, [this._description]);
-
-  int get id => _id;
-  String get title => _title;
-  String get description => _description;
-  String get date => _date;
-  int get priority => _priority;
-
-  set title(String newTitle) {
-    if (newTitle.length <= 255) {
-      this._title = newTitle;
-    }
-  }
-
-  set description(String newdescription) {
-    this._description = newdescription;
-  }
-
-  set priority(int newPriority) {
-    if (newPriority >= 1 && newPriority <= 3) {
-      this._priority = newPriority;
-    }
-  }
-
-  set date(String newDate) {
-    this._date = newDate;
-  }
-
-  // Convert note object to map object [for storing in DB]
-
-  Map<String, dynamic> toMap() {
-    var map = Map<String, dynamic>();
-
-    if (id != null) {
-      map["id"] = _id;
-    }
-    map["title"] = _title;
-    map["description"] = _description;
-    map["priority"] = _priority;
-    map["date"] = _date;
-
-    return map;
-  }
-
-  // Convert map object back to note object [to get data from db and use in dart]
-  Note.fromMapObject(Map<String, dynamic> map) {
-    this._id = map['id'];
-    this._title = map['title'];
-    this._description = map['description'];
-    this._priority = map['priority'];
-    this._date = map['date'];
-  }
+  Note(this.id, this.title, this.date, this.priority, this.description);
 }
