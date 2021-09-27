c49d7a4db3418facc2cf2c770a7ee0da9b7fc10a
Ankan Sharma <36896102+Ankan1998@users.noreply.github.com>
2021-09-27 16:22:41 +0530

    minor formatting

--- flutternote/lib/main.dart
+++ flutternote/lib/main.dart
@@ -1,9 +1,6 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:flutternote/models/note.dart';
 import 'package:flutternote/screens/note_detail.dart';
-//import 'package:flutternote/screens/note_detail.dart';
 import 'package:flutternote/screens/splash_screen.dart';
 import 'package:flutternote/themes.dart';
 import 'package:hive/hive.dart';
@@ -11,8 +8,6 @@ import 'package:path_provider/path_provider.dart' as path_provider;
 
 void main() {
   runApp(MyApp());
-  // // open the hive box
-  // final notebox = await Hive.openBox('notes');
 }
 
 class MyApp extends StatefulWidget {
